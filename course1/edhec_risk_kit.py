b20910324f5177a8392383392565c4b8a8e2c630
Ben Peterson <ben.peterson@pacificlife.com>
2021-02-04 22:06:10 +0000

    updated

--- course1/edhec_risk_kit.py
+++ course1/edhec_risk_kit.py
@@ -53,6 +53,18 @@ def skewness(r):
     exp = (demeaned_r**3).mean()
     return exp/sigma_r**3
 
+def kurtosis(r):
+    """
+    Alternative to scipy.stats.kurtosis()
+    Computes the kurtosis of the supplied Series or DataFrame
+    Returns a float or a Series
+    """
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**4).mean()
+    return exp/sigma_r**4
+
 def semideviation (r):
     """
     Returns the semideviation aka negative semideviation of r
@@ -73,4 +85,23 @@ def var_historic(r, level=5):
         return -np.percentile(r, level)
     else:
         raise TypeError("Expected to be Series or DataFrame")
+
+from scipy.stats import norm
+def var_gaussian(r, level=5, modified=False):
+    """
+    Returns the Parametric Gaussian VaR of a Series or DataFrame
+    """
+    # compute the Z score assuming it was Gaussian
+    z = norm.ppf(level/100)
+    if modified:
+        # modify the Z score based on observed skewness and kurtosis
+        s = skewness(r)
+        k = kurtosis(r)
+        z = (z +
+                (z**2 - 1)*s/6 +
+                (z**3 -3*z)*(k-3)/24 -
+                (2**z**3 - 5*z)*(s**2)/36
+            )
+        
+    return -(r.mean() + z*r.std(ddof=0))
 
