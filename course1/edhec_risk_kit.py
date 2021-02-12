0cc55c17e9426604ec2300f80b7b49b1627e3bb0
Ben Peterson <ben.peterson@pacificlife.com>
2021-02-12 15:58:52 +0000

    efficient frontiers

--- course1/edhec_risk_kit.py
+++ course1/edhec_risk_kit.py
@@ -126,3 +126,34 @@ def get_ind_returns():
     ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period('M')
     ind.columns = ind.columns.str.strip()
     return ind
+
+def annualize_vol(r, periods_per_year):
+    """
+    Annualizes the volatility of a set of returns
+    We should infer the periods per year
+    but that is currently left as an exercise
+    """
+    return r.std()*(periods_per_year**0.5)
+
+def annualize_rets(r, periods_per_year):
+    """
+    Annualizes a set of returns
+    We should infer the periods per year
+    but that is currently left as an exercise
+    """
+    compounded_growth = (1+r).prod()
+    n_periods = r.shape[0]
+    return compounded_growth**(periods_per_year/n_periods)-1
+
+def sharpe_ratio(r, riskfree_rate, periods_per_year):
+    """
+    Computes the annualized sharpe ratio of a set of returns
+    """
+    # convert the annual riskfree rate to per period
+    rf_per_period = (1+riskfree_rate)**(1/periods_per_year)-1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
+    ann_vol = annualize_vol(r, periods_per_year)
+    return ann_ex_ret/ann_vol
+
+    
