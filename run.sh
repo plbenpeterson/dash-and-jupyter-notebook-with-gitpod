80b36346204cce8611b29356f3e764f1db06ba2c
Ben Peterson <ben.peterson@pacificlife.com>
2021-02-16 15:01:51 +0000

    updating to install pip first

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 # install dependencies
+pip3 install --upgrade pip
 pip3 install dash # ==0.43.0 The core dash backend
 pip3 install dash-daq # ==0.1.0 DAQ components (newly open-sourced!)
 pip3 install jupyter pandas matplotlib scipy # install jupyter notebook, pandas(and dependant numpy), matplotlib
-pip3 install --upgrade pip
+
 
